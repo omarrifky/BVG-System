53a905ad1ac93cf2e4982635742116e721254b61
ziyadk1997 <ziyadk@windowslive.com>
2019-11-17 15:26:39 +0200

    Commented Code

--- Main.py
+++ Main.py
@@ -2,18 +2,11 @@
 parentPath = "/Users/ziko137/Documents/GitHub/BVG-System/"
 stationsTextFile = open(parentPath + "Stations.txt", "r")
 uBahnData = stationsTextFile.read()
-#data = uBahnData.split(",");
-#print(data);
-#print(uBahnData)
 
-###############################################
-# Python3 Program to print BFS traversal 
-# from a given source vertex. BFS(int s) 
-# traverses vertices reachable from s. 
+# Imports
 from collections import defaultdict 
 
-# This class represents a directed graph 
-# using adjacency list representation 
+# This class represents a Node Object
 class Node(object):
     #constructor
     def __init__(self,ID):
@@ -22,51 +15,43 @@ class Node(object):
         self.possibleMoves = []
         self.possibleMovesCost = []
         self.id = ID
-        
+# Graph Class has default dict of Nodes and the Traversals
 class Graph: 
 
 	# Constructor 
 	def __init__(self): 
-
 		# default dictionary to store graph 
 		self.graph = defaultdict(Node) 
         
-	# Function to print a BFS of graph 
+	# BFS Traversal 
 	def BFS(self,startNode,goalNode): 
 		# Create a queue for BFS 
 		queue = [] 
-        
-        
-		# Mark the source node as 
-		# visited and enqueue it 
+		#enqueue first startNode to start traversing BFS
 		queue.append(startNode)
-
+		# Initialize goal Node
 		goal = Node(-1)
-        
+        # traversing the queue until its empty
 		while queue: 
-			# Dequeue a vertex from 
-			# queue and print it 
+			# Dequeue an element
 			currentNode = queue.pop(0) 
-			# Get all adjacent vertices of the 
-			# dequeued vertex s. If a adjacent 
-			# has not been visited, then mark it 
-			# visited and enqueue it 
+			# check if it is a goal if it is break from the loop goal found
+			if goalNode == currentNode:
+				goal = self.graph[currentNode]
+				break
+			# Get possible Moves for current Node
 			possibleMovesArray = self.graph[currentNode].possibleMoves
-			for i in range(0,len(possibleMovesArray)):
-				if goalNode == possibleMovesArray[i]:
-					goal = self.graph[possibleMovesArray[i]]
-					goal.parentNode = self.graph[currentNode]
-					queue = []
-					break
-				if self.graph[possibleMovesArray[i]].visited == False: 
-					self.graph[possibleMovesArray[i]].parentNode = self.graph[currentNode]
-					queue.append(possibleMovesArray[i]) 
-					self.graph[possibleMovesArray[i]].visited = True
+			#Enqueue every possible move if not visited before
+			for possibleMove in possibleMovesArray:
+				if self.graph[possibleMove].visited == False: 
+					self.graph[possibleMove].parentNode = self.graph[currentNode]
+					queue.append(possibleMove) 
+					self.graph[possibleMove].visited = True
 
-		# goal Node now in Goal backtrack to get Path
-		#print(goal.id)
+		#If Goal Found
 		if not(goal.id ==-1):
 			pathInverted = []
+			#Tranverse Parents of Goal Node till start Node is reached and print path
 			while True:
 				pathInverted.append(goal)
 				if (goal.parentNode is None):
@@ -80,18 +65,20 @@ class Graph:
 
 			
 
-# Driver code 
+#Initialize An Array That has all stations (unique) 
 allStations = []
+#Remove \n from File and put each line in an element in an array
 uBahnData = uBahnData.replace("\n","")
 linesSeperated = uBahnData.split("#")
-
+#Fill all Stations (each station only occuring once)
 for line in linesSeperated:
 	lineSeperated = line.split(",")
 	for x in range (0,len(lineSeperated)-1,2):
          if not(allStations.__contains__(lineSeperated[x])):
              allStations.append(lineSeperated[x])
+#Create Graph
 g = Graph()
-
+#Add Edges with cost from each station to station
 for j in range(0,len(allStations)-1):
 	station = allStations[j]
 	stationNode = Node(j)
@@ -109,29 +96,9 @@ for j in range(0,len(allStations)-1):
 					stationNode.possibleMovesCost.append(abs(cost))
 	g.graph[j] = stationNode
 
-
-print(g.graph[2].possibleMoves)
-#g.BFS(0,3)
-	
-
-# Create a graph given in 
-# the above diagram 
-#g = Graph() 
-#g.addEdge(0, 1,5) 
-#g.addEdge(0, 2,6) 
-#g.addEdge(1, 2,7) 
-#g.addEdge(2, 0,8) 
-#g.addEdge(2, 3,9) 
-#g.addEdge(3, 3,10) 
+#DEBUGGING
 
 
+#print(g.graph[1].possibleMoves)
+#g.BFS(0,2)
 
-
-
-
-
-
-#print ("Following is Breadth First Traversal" " (starting from vertex 2)") 
-#g.BFS(2) 
-
-# This code is contributed by Neelam Yadav 
