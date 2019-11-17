7b47f4349475f6b79b898de2af04c50ae6fb5caa
ziyadk1997 <ziyadk@windowslive.com>
2019-11-17 21:01:57 +0200

    Added DFS

--- Main.py
+++ Main.py
@@ -22,21 +22,59 @@ class Graph:
 	def __init__(self): 
 		# default dictionary to store graph 
 		self.graph = defaultdict(Node) 
-        
-	# BFS Traversal 
+	
+	#DFS Traversal 
+	def DFS(self,startNode,goalNode): 
+		# Create a stack for DFS 
+		stack = [] 
+		#push first startNode to start traversing DFS
+		self.graph[startNode].visited = True
+		stack.append(startNode)
+		# Initialize goal Node
+		goal = Node(-1)
+        # traversing the stack until its empty
+		while stack: 
+			# pop an element
+			currentNode = stack.pop()
+			# check if it is a goal if it is break from the loop goal found
+			if int(goalNode) == int(currentNode):
+				goal = self.graph[currentNode]
+				break
+			# Get possible Moves for current Node
+			possibleMovesArray = self.graph[currentNode].possibleMoves
+			#push every possible move if not visited before
+			for possibleMove in possibleMovesArray:
+				if self.graph[possibleMove].visited == False: 
+					self.graph[possibleMove].parentNode = self.graph[currentNode]
+					stack.append(possibleMove) 
+					self.graph[possibleMove].visited = True
+		#If Goal Found
+		if not(goal.id ==-1):
+			pathInverted = []
+			#Tranverse Parents of Goal Node till start Node is reached and print path
+			while not (goal is None):
+				print(goal.id)
+				pathInverted.append(goal)
+				goal = goal.parentNode
+				
+			for x in pathInverted:
+				print(allStations[x.id])
+
+	#BFS Traversal 
 	def BFS(self,startNode,goalNode): 
 		# Create a queue for BFS 
 		queue = [] 
 		#enqueue first startNode to start traversing BFS
+		self.graph[startNode].visited = True
 		queue.append(startNode)
 		# Initialize goal Node
 		goal = Node(-1)
         # traversing the queue until its empty
 		while queue: 
 			# Dequeue an element
-			currentNode = queue.pop(0) 
+			currentNode = queue.pop(0)
 			# check if it is a goal if it is break from the loop goal found
-			if goalNode == currentNode:
+			if int(goalNode) == int(currentNode):
 				goal = self.graph[currentNode]
 				break
 			# Get possible Moves for current Node
@@ -47,20 +85,17 @@ class Graph:
 					self.graph[possibleMove].parentNode = self.graph[currentNode]
 					queue.append(possibleMove) 
 					self.graph[possibleMove].visited = True
-
 		#If Goal Found
 		if not(goal.id ==-1):
 			pathInverted = []
 			#Tranverse Parents of Goal Node till start Node is reached and print path
-			while True:
+			while not (goal is None):
+				print(goal.id)
 				pathInverted.append(goal)
-				if (goal.parentNode is None):
-					break
-				else:
-					goal = goal.parentNode
+				goal = goal.parentNode
 				
 			for x in pathInverted:
-				print(x.id)
+				print(allStations[x.id])
 
 
 			
@@ -97,8 +132,5 @@ for j in range(0,len(allStations)-1):
 	g.graph[j] = stationNode
 
 #DEBUGGING
-
-
 #print(g.graph[1].possibleMoves)
-#g.BFS(0,2)
-
+g.DFS(0,100)
