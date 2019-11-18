e5a96830b697b7f41eceda751892f8698f269af9
ziyadk1997 <ziyadk@windowslive.com>
2019-11-18 19:37:37 +0200

    Added Greedy

--- Main.py
+++ Main.py
@@ -2,15 +2,27 @@
 import os.path
 stationsTextFile = open(os.path.dirname(__file__)+"/Stations.txt", "r")
 uBahnData = stationsTextFile.read()
-
+stationsLatLangFile = open(os.path.dirname(__file__)+"/AllStationsLatLang.txt", "r")
+stationsLatLang = stationsLatLangFile.read().split("\n")
+latList = []
+longList = []
+for line in stationsLatLang:
+	if line == "":
+		break
+	lineSplited = line.split(",")
+	latList.append(float(lineSplited[1]))
+	longList.append(float(lineSplited[2]))
+R = 6373.0
 # Imports
-from collections import defaultdict 
+from collections import defaultdict
+from math import sin, cos, sqrt, atan2, radians
 
 # This class represents a Node Object
 class Node(object):
     #constructor
     def __init__(self,ID):
         self.visited = False
+        self.heuristic = 0.0
         self.parentNode = None
         self.possibleMoves = []
         self.possibleMovesCost = []
@@ -22,7 +34,69 @@ class Graph:
 	def __init__(self): 
 		# default dictionary to store graph 
 		self.graph = defaultdict(Node) 
-	
+	#Greedy Traversal
+	def Greedy(self,startNode,goalNode):
+		#create list to put Nodes in
+		list = []
+		#add first Node and mark as visited
+		self.graph[startNode].visited = True
+		list.append(self.graph[startNode])
+		lat1 = latList[startNode]
+		lon1 = longList[startNode]
+		lat2 = latList[goalNode]
+		lon2 = longList[goalNode]
+		dlon = lon2 - lon1
+		dlat = lat2 - lat1
+		a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+		c = 2 * atan2(sqrt(a), sqrt(1 - a))
+		self.graph[startNode].heuristic = R * c
+		#Initialize Goal Node
+		goal = Node(-1)
+		#while list not empty
+		while list:
+			#remove element with lowest heuristic cost
+			currentNode = list[0]
+			for node in list:
+				if node.heuristic < currentNode.heuristic:
+					currentNode = node
+			list.remove(currentNode)
+			#check if goal
+			if goalNode == currentNode.id:
+				goal = self.graph[currentNode.id]
+				break
+			# Get possible Moves for current Node
+			possibleMovesArray = self.graph[currentNode.id].possibleMoves
+			#push every possible move if not visited before
+			for possibleMove in possibleMovesArray:
+				if self.graph[possibleMove].visited == False:
+					lat1 = latList[possibleMove]
+					lon1 = longList[possibleMove]
+					lat2 = latList[goalNode]
+					lon2 = longList[goalNode]
+					dlon = lon2 - lon1
+					dlat = lat2 - lat1
+					a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+					c = 2 * atan2(sqrt(a), sqrt(1 - a))
+					self.graph[possibleMove].heuristic = R * c
+					print(self.graph[possibleMove].heuristic)
+					self.graph[possibleMove].parentNode = self.graph[currentNode.id]
+					list.append(self.graph[possibleMove]) 
+					self.graph[possibleMove].visited = True
+		#If Goal Found
+		if not(goal.id ==-1):
+			pathInverted = []
+			#Tranverse Parents of Goal Node till start Node is reached and print path
+			while not (goal is None):
+				print(goal.id)
+				pathInverted.append(goal)
+				goal = goal.parentNode
+				
+			for x in pathInverted:
+				print(allStations[x.id])
+
+
+
+
 	#DFS Traversal 
 	def DFS(self,startNode,goalNode): 
 		# Create a stack for DFS 
@@ -133,9 +207,5 @@ for j in range(0,len(allStations)-1):
 
 #DEBUGGING
 #print(g.graph[1].possibleMoves)
-#g.DFS(0,100)
+g.Greedy(0,100)
 
-f = open("AllStations3.txt", "w")
-for y in allStations:
-	f.write(y + "#")
-f.close()
